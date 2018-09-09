8686ccae134f5b5b7ba737665c9b1d1ffbce09b6
Sourabhtripathi <sourabh.tripathi48@gmail.com>
2018-09-10 01:48:21 +0530

    add authentication routes

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const app = express();
 const clubRoutes = require("./routes/club");
+const indexRoutes = require("./routes/index");
 
 app.set("view engine", "ejs");
 
 
 app.use(clubRoutes);
-
-
-
+app.use(indexRoutes);
 
 app.listen(3000, function(){
     console.log("Application has started!!");
--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,32 @@
-var express = require("express"),
-	router = express.Router();
+const express = require("express");
+const router = new express.Router();
 
 // Landing
 router.get("/", function(req, res){
-	res.send("Landing Page");
+    res.render("index/landing");
 });
 
 // // Register
-// router.get("/register", function(req, res){
-// 	res.send("register form");
-// });
+router.get("/register", function(req, res){
+	res.render("index/register");
+});
 
-// router.post("/register", function(req, res){
-// 	res.send("register post route");
-// });
+router.post("/register", function(req, res){
+	res.send("register post route");
+});
 
 // // Login
-// router.get("/login", function(req, res){
-// 	res.send("login form");
-// });
+router.get("/login", function(req, res){
+	res.render("index/login");
+});
 
-// router.post("/login", function(req, res){
-// 	res.send("login post route");
-// });
+router.post("/login", function(req, res){
+	res.send("login post route");
+});
 
-// // Logout
+// Logout
+router.get("/logout", function(req, res){
+	res.send("Logout route");
+});
+
+module.exports = router;
