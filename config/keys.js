3508bfb4d981e8d64262ee7180581dba708696a1
Kristency <nandisubhajit666@gmail.com>
2018-10-05 13:13:29 +0530

    removed local strategies

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    google: {
+        clientID: "269856010184-10bn3gnl81ouvqr46fmj60b4rjkibg2t.apps.googleusercontent.com",
+        clientSecret: "bAaRmI0QWOZqKd1zMw-yzTle"
+    },
+    
+}
