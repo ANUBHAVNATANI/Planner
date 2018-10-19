1a371f6e56ffd963e0c3998d261fc9446adec904
Sourabhtripathi <sourabh.tripathi48@gmail.com>
2018-10-19 19:27:56 +0530

    final user schema

--- models/user.js
+++ models/user.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    
+    username: String,
+    googleId: String,
+    thumbnail: String,
+    clubs: [String],
+    batch: String,
+    email: String
 });
 
 const User = mongoose.model('user', userSchema);
