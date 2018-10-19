981dc218f2ab915d5aa1f70d6dc04166041d236e
Sourabhtripathi <sourabh.tripathi48@gmail.com>
2018-10-19 19:22:39 +0530

    change user schema

--- models/user.js
+++ models/user.js
@@ -5,7 +5,9 @@ const userSchema = new Schema({
     username: String,
     googleId: String,
     thumbnail: String,
-    year: String
+    year: String,
+    batch : String,
+    clubs : [String]
 });
 
 const User = mongoose.model('user', userSchema);
